bbff7f2202952f47a2a6dd34568f5ff062e00a5e
AhmedAbdelazeem98 <55737870+AhmedAbdelazeem98@users.noreply.github.com>
2024-01-25 23:23:54 +0200

    settings routes enhanced

---
+++ routes/admin.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Http\Controllers\Dashboard\IndexController;
+use App\Http\Controllers\Dashboard\SettingsController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+Route::get('/admin', [IndexController::class, 'index'])-> name('admin');
+
+
+#############   Settings Routes    ############# 
+Route::get('/settings', [SettingsController::class, 'index'])-> name('dashboard.settings.view');
+Route::put('/settings/{setting}', [SettingsController::class, 'update'])-> name('dashboard.settings.update');
+
+#############     End Setttings routes ###########
+
+
